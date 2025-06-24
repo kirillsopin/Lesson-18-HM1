481dd3088d4c5bfb5c504c921787e8607c847f80
kirillsopin <sopinkirill001@gmail.com>
2025-06-24 23:17:53 +0300

    shows everyone over 60

--- Lesson-18-HM1.cpp
+++ Lesson-18-HM1.cpp
@@ -24,17 +24,43 @@ int main() {
     setlocale(LC_ALL, "");
 
     Pracivnyk pracivnyky[5] = {
-        {"Іваненко", 1985, "Менеджер", 15000.50, "Вища"},
+        {"Іваненко", 1950, "Менеджер", 15000.50, "Вища"},
         {"Петренко", 1990, "Програміст", 25000.00, "Вища"},
-        {"Сидоренко", 1982, "Бухгалтер", 14000.75, "Середня спеціальна"},
+        {"Сидоренко", 1960, "Бухгалтер", 14000.75, "Середня спеціальна"},
         {"Мельник", 1995, "Консультант", 10000.00, "Вища"},
         {"Коваль", 1988, "Системний адміністратор", 18000.25, "Вища"}
     };
 
+    const int potocnyRik = 2025;
+    int count = 0;
+
+    cout << "Усі працівники:\n";
     for (int i = 0; i < 5; ++i) {
         pokazatyInfo(pracivnyky[i]);
     }
 
+    cout << "\nПрацівники, старші за 60 років:\n";
+    for (int i = 0; i < 5; ++i) {
+        int vik = potocnyRik - pracivnyky[i].rik_narodzhennia;
+        if (vik > 60) {
+            pokazatyInfo(pracivnyky[i]);
+            ++count;
+        }
+    }
+
+    cout << "\nПрізвища працівників, старших за 60 років:\n";
+    for (int i = 0; i < 5; ++i) {
+        int vik = potocnyRik - pracivnyky[i].rik_narodzhennia;
+        if (vik > 60) {
+            cout << "- " << pracivnyky[i].prizvyshche << endl;
+        }
+    }
+
+    cout << "Загальна кількість працівників старших за 60 років: " << count << endl;
+
     return 0;
 }
+
+
+
 
