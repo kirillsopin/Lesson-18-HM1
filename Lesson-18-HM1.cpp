fc5e5592f88531a264841b541b3feacdcd247bb3
kirillsopin <sopinkirill001@gmail.com>
2025-06-24 23:11:46 +0300

    Add 5 masiv

--- Lesson-18-HM1.cpp
+++ Lesson-18-HM1.cpp
@@ -3,8 +3,6 @@
 
 using namespace std;
 
-
-
 struct Pracivnyk {
     string prizvyshche;
     int rik_narodzhennia;
@@ -19,21 +17,24 @@ void pokazatyInfo(const Pracivnyk& p) {
     cout << "Посада: " << p.posada << endl;
     cout << "Зарплата: " << p.zarplata << endl;
     cout << "Освіта: " << p.osvita << endl;
+    cout << "--------------------------" << endl;
 }
 
 int main() {
     setlocale(LC_ALL, "");
-    
 
-    Pracivnyk pracivnyk1 = {
-        "Іваненко",
-        1985,
-        "Менеджер",
-        15000.50,
-        "Вища"
+    Pracivnyk pracivnyky[5] = {
+        {"Іваненко", 1985, "Менеджер", 15000.50, "Вища"},
+        {"Петренко", 1990, "Програміст", 25000.00, "Вища"},
+        {"Сидоренко", 1982, "Бухгалтер", 14000.75, "Середня спеціальна"},
+        {"Мельник", 1995, "Консультант", 10000.00, "Вища"},
+        {"Коваль", 1988, "Системний адміністратор", 18000.25, "Вища"}
     };
 
-    pokazatyInfo(pracivnyk1);
+    for (int i = 0; i < 5; ++i) {
+        pokazatyInfo(pracivnyky[i]);
+    }
 
     return 0;
 }
+
